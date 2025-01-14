792a33823503ec50ee6faaf49db29a6fe0d5c8f1
Udhaykrishn <krishnauday320@gmail.com>
2025-01-14 09:58:56 +0530

    feat(main): main file added the database connect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { app } from "@/app";
+import app from "@/app"
+import { connectDB } from "./config";
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT as string;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)) 
+const bootstrap = async () => {
+    await connectDB();
+    app.listen(PORT, () => console.log(`🚀 Server started on ${PORT}`))
+}
+
+bootstrap(); 
